4efd3f1e6b13971426245fb8995a513c92bc5e80
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-03-26 04:41:58 +0600

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,21 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
+
+## Fill these variables if you're deploying on heroku.
+# Your heroku app name
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+# Get it from http://dashboard.heroku.com/account
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/Adnan69x/Scrape/",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+)  # Fill this variable if your upstream repository is private
+
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AdnansMusic")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AdnansMusicSupports")
